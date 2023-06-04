e6d0fc289d20a36c8a8973607982e8fe396b4b0b
jeffin-Dev <135037032+jeffin-Dev@users.noreply.github.com>
2023-06-04 14:39:18 -0300

    First Deploy

---
+++ Automacao WP- gui tkinter/Proxlight_Designer_Export/functionwindow.py
@@ -0,0 +1,73 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+import urllib.parse
+
+
+class Navegador:
+    def __init__(self):
+        self.numero = None
+        self.mensagem = None
+
+    def cliquei_no_link(self, numero, mensagem):
+        servico = Service(ChromeDriverManager().install())
+        driver = webdriver.Chrome(service=servico)
+        texto = urllib.parse.quote(mensagem)
+
+        driver.get(f'https://web.whatsapp.com/')
+        # time.sleep(30)
+
+        # esperar gerar qrcode
+        while len(driver.find_elements(By.XPATH, '//*[@id="app"]/div/div/div[3]/div[1]/div/div/div[2]/div/canvas')) < 1:
+            time.sleep(1)
+
+        # esperar carregar whats
+        while len(driver.find_elements(By.XPATH, '//*[@id="app"]/div/div/div[5]/div/div/div[2]/div[1]/h1')) < 1:
+            time.sleep(1)
+        time.sleep(7)
+        print('WhatsApp logado e carregado com sucesso!\nProxima etapa..')
+
+        # numeros = ['31973093105','31985704347', '31989015177']
+        # for numero in numeros:
+            # print (numero)
+
+
+        # -------------------- INICIO DO DESPARO  --------------------
+
+        # abrir cvs com a pessoa
+        print(f'Abrindo conversa com (AQUI COLOCAR O NOME DO CLIENTE)')
+        driver.get(f'https://api.whatsapp.com/send/?phone=55{numero}&text={texto}&type=phone_number&app_absent=0')
+        time.sleep(5)
+
+        # clicar em inciar conversa
+        while len(driver.find_elements(By.XPATH,'//*[@id="action-button"]')) < 1:
+            time.sleep(1)
+        time.sleep(1)
+        driver.find_element(By.XPATH, '//*[@id="action-button"]').click()
+
+        # abrir whats web
+        while len(driver.find_elements(By. XPATH,'//*[@id="fallback_block"]/div/div/h4[2]/a')) < 1:
+            time.sleep(1)
+        time.sleep(1)
+        driver.find_element(By.XPATH, '//*[@id="fallback_block"]/div/div/h4[2]/a').click()
+
+        # Enviar mensagem
+        while len(driver.find_elements(By.XPATH,'//*[@id="main"]/footer/div'
+                                                '[1]/div/span[2]/div/div[2]/div[1]/div/div[1]')) < 1:
+            time.sleep(1)
+        time.sleep(2)
+        driver.find_element(By.XPATH, '//*[@id="main"]/footer/div'
+                                                '[1]/div/span[2]/div/div[2]/div[1]/div/div[1]').send_keys(Keys.ENTER)
+        print('mensagem enviada')
+        time.sleep(5)
+
+if '__main__' == __name__:
+
+    a= Navegador()
+    a.cliquei_no_link('31973093105','teste')
+
+
+
---
+++ Automacao WP- gui tkinter/Proxlight_Designer_Export/window.py
@@ -0,0 +1,77 @@
+from tkinter import *
+from functionwindow import Navegador
+
+driver = Navegador()
+
+class EnvioDeWhatsapp:
+    def __init__(self):
+        self.window = Tk()
+        self.window.title('Envio de mensagem pelo WhatsApp')
+        self.window.geometry("720x439")
+        self.window.configure(bg = "#d8d8d8")
+        self.canvas = Canvas(
+            self.window,
+            bg = "#d8d8d8",
+            height = 439,
+            width = 720,
+            bd = 0,
+            highlightthickness = 0,
+            relief = "ridge")
+        self.canvas.place(x = 0, y = 0)
+
+        self.background_img = PhotoImage(file = f"background.png")
+        self.background = self.canvas.create_image(
+            394.5, 228.0,
+            image=self.background_img)
+
+        self.entry0_img = PhotoImage(file = f"img_textBox0.png")
+        self.entry0_bg = self.canvas.create_image(
+            550.5, 287.5,
+            image = self.entry0_img)
+
+        self.mensagem_para_envio = Text(
+            bd = 0,
+            bg = "#ffffff",
+            highlightthickness = 0)
+        self.mensagem_para_envio.place(
+            x = 425, y = 231,
+            width = 255,
+            height = 115)
+
+        self.entry1_img = PhotoImage(file = f"img_textBox1.png")
+        self.entry1_bg = self.canvas.create_image(
+            544.5, 158.0,
+            image = self.entry1_img)
+
+        self.telefone_para_envio = Entry(
+            bd = 0,
+            bg = "#ffffff",
+            highlightthickness = 0)
+        self.telefone_para_envio.place(
+            x = 453, y = 145,
+            width = 183,
+            height = 26)
+
+        self.img0 = PhotoImage(file = f"img0.png")
+        self.botao_enviar = Button(
+            text='Enviar',
+            bg='#ACFFB9',
+            fg='black',
+            borderwidth = 5,
+            highlightthickness = 5,
+            command = self.cliando_em_enviar,
+            relief = "flat")
+        self.botao_enviar.place(
+            x = 501, y = 371,
+            width = 116,
+            height = 37)
+
+        self.window.resizable(False, False)
+        self.window.mainloop()
+
+    def cliando_em_enviar(self):
+        driver.cliquei_no_link(self.telefone_para_envio.get(), self.mensagem_para_envio.get('1.0', END))
+
+
+if '__main__' == __name__:
+    a = EnvioDeWhatsapp()
